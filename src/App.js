49cce393ccd776108d808d79f24a737d522182c1
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-30 16:55:13 +0530

    Added Redux Store and Reducer to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import {createStore} from 'redux';
 
+import reducer from './store/reducer';
 import Counter from './containers/Counter/Counter';
 import './App.css';
+
+const store  = createStore(reducer);
 
 class App extends Component {
   render() {
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+    counter:0
+}
+
+const reducer = (state = initialState,action) => {
+
+    return {...state};
+
+}
+
+export default reducer;
