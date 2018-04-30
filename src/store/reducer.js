dcdc246048ba7a026506a3a89f2a1b2e1c905ff2
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-30 18:08:10 +0530

    Added Dispatch Hnadler to all Function and created dispatch action listeners in store/reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,29 @@ const initialState = {
 
 const reducer = (state = initialState,action) => {
 
+
+    if(action.type === 'INCREMENT')
+    {
+        return {...state,counter:state.counter+1};
+    }
+
+    if(action.type === 'DECREMENT')
+    {
+        return {...state,counter:state.counter-1};
+    }
+
+    if(action.type === 'ADD')
+    {
+        return {...state,counter:state.counter+action.payload.value};
+    }
+
+    if(action.type === 'SUBSTRACT')
+    {
+        return {...state,counter:state.counter-action.payload.value};
+    }
+
+
+
     return {...state};
 
 }
