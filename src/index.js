6f6a47cb399d2c35314f46cdacea959969659725
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-02 08:27:14 +0530

    Splitted reducers for scalabilty and combined to central store using combineReducer function provided by redux;

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './store/reducer';
+import {combineReducers} from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import counterReducer from './store/reducers/counterReducer';
+import snapshotsReducer from './store/reducers/snapshotReducer';
 
 
-const store  = createStore(reducer);
+const rootReducer = combineReducers(
+    {
+        counter:counterReducer,
+        snapshots:snapshotsReducer,
+    }
+)
+const store  = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducers/counterReducer.js
@@ -0,0 +1,25 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    counter: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case actionTypes.INCREMENT:
+            return { ...state, counter: state.counter + 1 };
+        case actionTypes.DECREMENT:
+            return { ...state, counter: state.counter - 1 };
+        case actionTypes.ADD:
+            return { ...state, counter: state.counter + action.payload.value };
+        case actionTypes.SUBSTRACT:
+            return { ...state, counter: state.counter - action.payload.value };
+        default:
+            return { ...state };
+            break;
+    }
+
+}
+
+export default counterReducer;
