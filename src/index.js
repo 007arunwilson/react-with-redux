d28cff895d64e3a25c9c190c1d4d24df6822901b
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-04 11:54:00 +0530

    Added redux thunk functionality to application; Now 'SAVECOUNTERSNAPSHOT' action takes 2 secs to update the state ( to action take place ) ;

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware,combineReducers,compose} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -38,7 +39,7 @@ const logger = store => {
 
 const enhanceComposer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store  = createStore(rootReducer,enhanceComposer(applyMiddleware(logger)));
+const store  = createStore(rootReducer,enhanceComposer(applyMiddleware(logger,thunk)));
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
