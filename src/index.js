eabd0ef345998e8dbca40cfcc19b0b70d4e380af
Arun Wilson <007arunwilson@gmail.com>
2018-09-04 18:12:56 +0530

    redux-saga added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './store/reducer';
+import { combineReducers,applyMiddleware } from 'redux';
+import createSagaMiddleware from "redux-saga";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-const store  = createStore(reducer);
+const rootReducer = combineReducers(
+    {
+        counter: counterReducer,
+        snapshots: snapshotsReducer,
+    }
+)
+const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
