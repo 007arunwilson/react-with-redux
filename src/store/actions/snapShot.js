88be0f31eb57add733c95c4d4e6d5e9069e23256
Arun Wilson <007arunwilson@gmail.com>
2018-09-10 15:42:41 +0530

    Removed delay of dispatch for testability

--- src/store/actions/snapShot.js
+++ src/store/actions/snapShot.js
@@ -1,33 +1,17 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 
-export const saveCounterSnapShot = (payload) => {
-
-    return function (dispatch,getState) {
-
-        const promiseInstance = new Promise(resolve => {
-            setTimeout(() => {
-                resolve();
-            }, 2000);
-        });
-
-        promiseInstance.then(()=>{
-
-            console.log('[State snapshots before update]',getState().snapshots)
-
-            return dispatch(
-            {
-                type: actionTypes.SAVECOUNTERSNAPSHOT,
-                payload: payload
-            });
-        });
-
-    }
+export const saveCounterSnapShot = payload => {
+  return function(dispatch, getState) {
+    return dispatch({
+      type: actionTypes.SAVECOUNTERSNAPSHOT,
+      payload: payload
+    });
+  };
 };
 
-export const deleteCounterSnapShot = (payload) => {
-
-    return {
-        type: actionTypes.DELETECOUNTERSNAPSHOT,
-        payload: payload,
-    }
+export const deleteCounterSnapShot = payload => {
+  return {
+    type: actionTypes.DELETECOUNTERSNAPSHOT,
+    payload: payload
+  };
 };
